262711e1c492d7ae808f1449343d67a9705840c0
DeivydHXS <deivydhomam1@gmail.com>
2022-02-10 18:00:31 -0300

    middleware auth

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,23 +12,24 @@ import { DeleteUserController } from "./controllers/User/DeleteController";
 import { GetAllUsersController } from "./controllers/User/GetAllController";
 import { LoginController } from "./controllers/User/LoginController";
 import { UpdateUserController } from "./controllers/User/UpdateController";
+import { auth } from "./middlewares/auth";
 
 const routes = Router();
 
-routes.post('/categories', new CreateCategoryController().handle);
-routes.get('/categories', new GetAllCategoriesController().handle);
-routes.delete('/categories/:id', new DeleteCategoryController().handle);
-routes.put('/categories/:id', new UpdateCategoryController().handle);
+routes.post('/categories', auth, new CreateCategoryController().handle);
+routes.get('/categories', auth, new GetAllCategoriesController().handle);
+routes.delete('/categories/:id', auth, new DeleteCategoryController().handle);
+routes.put('/categories/:id', auth, new UpdateCategoryController().handle);
 
-routes.post('/books', new CreateBookController().handle);
-routes.get('/books', new GetAllBooksController().handle);
-routes.delete('/books/:id', new DeleteBookController().handle);
-routes.put('/books/:id', new UpdateBookController().handle);
+routes.post('/books', auth, new CreateBookController().handle);
+routes.get('/books', auth, new GetAllBooksController().handle);
+routes.delete('/books/:id', auth, new DeleteBookController().handle);
+routes.put('/books/:id', auth, new UpdateBookController().handle);
 
 routes.post('/users', new CreateUserController().handle);
-routes.get('/users', new GetAllUsersController().handle);
-routes.delete('/users/:id', new DeleteUserController().handle);
-routes.put('/users/:id', new UpdateUserController().handle);
+routes.get('/users', auth, new GetAllUsersController().handle);
+routes.delete('/users/:id', auth, new DeleteUserController().handle);
+routes.put('/users/:id', auth, new UpdateUserController().handle);
 routes.post('/session', new LoginController().handle);
 
 
